7779d65a22ce857858d6b07464e6da0b1c352c84
RevREB <richardebaker@gmail.com>
2014-02-16 17:12:43 -0800

    menu clean up

--- header.inc.php
+++ header.inc.php
@@ -36,25 +36,6 @@ $page = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
 	</div>
 
 <div id="templatemo_wrapper">	
-<div id="templatemo_header">
-    <div id="site_title"><h1><a href="index.php">Main</a></h1></div>
-
-    <!--<div id="templatemo_menu" class="ddsmoothmenu">
-      <ul>
-<?php
-      foreach ($pages as $key => $value)
-      {
-        if  ($value == $page)
-          $selected = "class='selected'";
-        else
-          $selected = "";
-
-        echo "<li><a href='".$value."' ".$selected.">".$key."</a></li>";
-      }
-?>
-    </ul>
-    <br style="clear: left" />
-  </div> --> <!-- end of templatemo_menu -->
-        
-</div> <!-- end of header -->
+      
+</div> --> <!-- end of header -->
 
