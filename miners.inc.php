d76e492f5be5f73de4859140367a85e640cef7ba
RevREB <richardebaker@gmail.com>
2014-02-17 14:25:55 -0800

    panel model test

--- miners.inc.php
+++ miners.inc.php
@@ -6,12 +6,18 @@
 					<h5>Miner Name</h5>
 					<p>5s rate</p>
 					<p>Avg rate</p>
-					<a href="#" class="tiny secondary radius button split">Split Button <span data-dropdown="paneldrop"></span></a><br>
+					<a href="#" class="small secondary radius button split">Actions<span data-dropdown="paneldrop"></span></a><br>
 						<ul id="paneldrop" class="f-dropdown" data-dropdown-content>
 							<li><a href="#">Start</a></li>
 							<li><a href="#">Start</a></li>
 							<li><a href="#">Restart</a></li>
+							<li><a href="#" data-reveal-id="panelModal0" data-reveal>Expand</a></li>
 						</ul>
+						<div id="panelModal0" class="reveal-modal" data-reveal>
+							<h5>Add a Miner</h5>
+							<iframe name="inlineframe" src="addhost.mod.php" frameborder="0" scrolling="auto" width="100%" height="50%" marginwidth="5" marginheight="5" ></iframe>
+							<a class="close-reveal-modal">&#215;</a>
+						</div>
 				</div>
 			</div>
 			<div class="small-4 columns">
