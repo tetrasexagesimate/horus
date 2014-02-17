dafbe9b76b444c07e0e166fad29b3b2eb0d11a4e
RevREB <richardebaker@gmail.com>
2014-02-17 14:03:27 -0800

    panel button id change

--- miners.inc.php
+++ miners.inc.php
@@ -3,15 +3,15 @@
 		<div class="row">
 			<div class="small-4 columns">
 				<div class="panel">
-				<h5>Miner Name</h5>
-				<p>5s rate</p>
-				<p>Avg rate</p>
-				<a href="#" class="tiny secondary radius button split">Split Button <span data-dropdown="drop"></span></a><br>
-					<ul id="drop" class="f-dropdown" data-dropdown-content>
-						<li><a href="#">Start</a></li>
-						<li><a href="#">Start</a></li>
-						<li><a href="#">Restart</a></li>
-					</ul>
+					<h5>Miner Name</h5>
+					<p>5s rate</p>
+					<p>Avg rate</p>
+					<a href="#" class="tiny secondary radius button split">Split Button <span data-dropdown="drop"></span></a><br>
+						<ul id="paneldrop" class="f-dropdown" data-dropdown-content>
+							<li><a href="#">Start</a></li>
+							<li><a href="#">Start</a></li>
+							<li><a href="#">Restart</a></li>
+						</ul>
 				</div>
 			</div>
 			<div class="small-4 columns">
