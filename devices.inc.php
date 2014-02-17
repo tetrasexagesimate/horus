0158ec5ff95ef182b6275c06665c30161d5ce7d3
RevREB <richardebaker@gmail.com>
2014-02-16 23:22:19 -0800

    Tabby Wabby 8

--- devices.inc.php
+++ devices.inc.php
@@ -1,4 +1,4 @@
-<div class="large-9 columns" role="content">
+<div class="small-12 columns" role="content">
 		<div class="row">
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
--- miners.inc.php
+++ miners.inc.php
@@ -1,3 +1,4 @@
+<div class="small-12 columns" role="content">
 		<div class="row">
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
@@ -33,4 +34,4 @@
 				<p>Bacon</p>
 			</div>
 		</div>
-    
+    </div>
