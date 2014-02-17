db49b1ab122606ec93c4677835285d9f2e2ecb0c
RevREB <richardebaker@gmail.com>
2014-02-16 23:10:32 -0800

    Tabby Wabby 3

--- miners.inc.php
+++ miners.inc.php
@@ -1,4 +1,3 @@
-<div class="large-9 columns" role="content">
 		<div class="row">
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
@@ -34,39 +33,4 @@
 				<p>Bacon</p>
 			</div>
 		</div>
-    </div>		<div class="row">
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-		</div>
-     <hr />
-		<div class="row">
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-			<div class="small-4 columns">
-				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
-			</div>
-		</div>
-    </div>
+    
