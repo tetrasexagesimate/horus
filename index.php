2d1636ecdd2e1989d8d35733f55f74d09475ade9
RevREB <richardebaker@gmail.com>
2014-02-16 16:40:58 -0800

    corrected foundation js typos

--- index.php
+++ index.php
@@ -35,7 +35,8 @@ $config = get_config_data();
 <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
 
 <script type="text/javascript" src="scripts/jquery.min.js"></script>
-<script src="/js/vendor/modernizr.js"></script>
+<script src="js/vendor/modernizr.js"></script>
+<script src="js/foundation.min.js"></script>
 <script type="text/javascript" src="scripts/ddsmoothmenu.js">
 
 
@@ -122,6 +123,9 @@ else
 
 <script src="/js/vendor/jquery.js"></script>
 <script src="/js/vendor/fastclick.js"></script>
+<script>
+  $(document).foundation();
+</script>
 <script>
 $(function() {
   setInterval(update, 5000);
