0e5306fbb69782adeea62e0c7713a8f6a0fd3acc
RevREB <richardebaker@gmail.com>
2014-02-17 00:52:25 -0800

    Add New Miner Modal

--- miners.inc.php
+++ miners.inc.php
@@ -3,36 +3,36 @@
 		<div class="row">
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
 			</div>
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
 			</div>
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
 			</div>
 		</div>
      <hr />
 		<div class="row">
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
 			</div>
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
 			</div>
 			<div class="small-4 columns">
 				<img src="http://placehold.it/400x300&text=[img]" />
-				<h4>This is a content section.</h4>
-				<p>Bacon</p>
+			</div>
+		</div>
+	<hr />
+		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
 			</div>
 		</div>
     </div>
