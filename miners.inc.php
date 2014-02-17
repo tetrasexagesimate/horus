abb7695550c7db3b6427d6e1e7d0ddf5ec9d06fc
RevREB <richardebaker@gmail.com>
2014-02-17 13:48:02 -0800

    Panel Formating

--- miners.inc.php
+++ miners.inc.php
@@ -3,16 +3,16 @@
 		<div class="row">
 			<div class="small-4 columns">
 				<div class="panel">
-				<h5>SHA-256 Hashrate</h5>
+				<h5>Miner Name</h5>
 				<p>5s rate</p>
-				<p>Avg</p>
-				</div>
-				<div class="button-bar">
-					<ul class="button-group round">
-						<li><a href="#" class="tiny button secondary">Start</a></li>
-						<li><a href="#" class="tiny button secondary">Stop</a></li>
-						<li><a href="#" class="tiny button secondary">Restart</a></li>
-					</ul>
+				<p>Avg rate</p>
+					<div class="button-bar">
+						<ul class="button-group round">
+							<li><a href="#" class="tiny button secondary">Start</a></li>
+							<li><a href="#" class="tiny button secondary">Stop</a></li>
+							<li><a href="#" class="tiny button secondary">Restart</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 			<div class="small-4 columns">
