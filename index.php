52466b3bd8f2b89be8d7d9f5fd3cb59c5da4ca66
RevREB <richardebaker@gmail.com>
2014-02-16 22:29:06 -0800

    Card style display

--- index.php
+++ index.php
@@ -132,7 +132,23 @@ $config = get_config_data();
  
     <!-- Main Blog Content -->
     <div class="large-9 columns" role="content">
- 
+		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+		</div>
       
  
       <hr />
