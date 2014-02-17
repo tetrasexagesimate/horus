3e9fa5cd489a205ec8794aad371c00929e758c30
RevREB <richardebaker@gmail.com>
2014-02-16 23:09:17 -0800

    Tabby Wabby 2

---
+++ miners.inc.php
@@ -0,0 +1,72 @@
+<div class="large-9 columns" role="content">
+		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+		</div>
+     <hr />
+		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+		</div>
+    </div>		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+		</div>
+     <hr />
+		<div class="row">
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+			<div class="small-4 columns">
+				<img src="http://placehold.it/400x300&text=[img]" />
+				<h4>This is a content section.</h4>
+				<p>Bacon</p>
+			</div>
+		</div>
+    </div>
