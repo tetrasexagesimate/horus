9871bb6f52c4d4ff6447e12cb4fed0830d065ed6
RevREB <richardebaker@gmail.com>
2014-02-17 17:23:46 -0800

    interesting

--- charts.inc.php
+++ charts.inc.php
@@ -1,13 +1,15 @@
-<div class="row" data-equalizer-watch>
+<div class="row">
 	<div class="small-12 columns">
-		<div class="row" data-equalizer-watch>
+		<div class="row">
 			<div class="small-6 columns">
-				<div class="panel" data-equalizer-watch>
+				<div class="panel">
 					<p>5s circle</p>
+					<p>Pie chart of all active Miners</p>
+					<p>http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/pie/search_engine_share/</p>
 				</div>
 			</div>
 			<div class="small-6 columns">
-				<div class="panel" data-equalizer-watch>
+				<div class="panel">
 					<p>Charts will go here</p>
 					<p>Charts will go here</p>
 					<p>Charts will go here</p>
@@ -16,9 +18,13 @@
 				</div>
 			</div>
 		</div>
-		<div class="row" data-equalizer-watch>
-			<div class="panel">
-				<p>24h stack chart</p>
+		<div class="row">
+			<div class="small-12 columns">
+				<div class="panel">
+					<p>24h stack chart</p>
+					<p>This will be a stacked area chart.</p>
+					<p>http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/area/productivity_by_day/</p>
+				</div>
 			</div>
 		</div>
     </div>
