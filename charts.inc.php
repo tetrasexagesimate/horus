90fd19e56400f17bad33d695ded00bb02455a645
RevREB <richardebaker@gmail.com>
2014-02-17 17:54:07 -0800

    its working!

--- charts.inc.php
+++ charts.inc.php
@@ -83,94 +83,82 @@
 								}
 							},
 							data: [
-              {  
-                name: "very distracting",
-                showInLegend: true,
-                legendMarkerType: "square",
-                type: "stackedArea",
-                color :"rgba(211,19,14,.8)",
-                markerSize: 0,
-
-                dataPoints: [
-                {x: new Date(2013, 02 , 17) , y: 2.4  },
-                {x: new Date(2013, 02 , 18) , y: .6  },
-                {x: new Date(2013, 02 , 19) , y: .8  },
-                {x: new Date(2013, 02 , 20) , y: 1.6  },
-                {x: new Date(2013, 02 , 21) , y: 1.4  },
-                {x: new Date(2013, 02 , 22) , y: 1.4  },
-                {x: new Date(2013, 02 , 23) , y: 2.6  }
-
-                ]
-            },
-              {        
-                name: "distracting",
-                showInLegend: true,
-                legendMarkerType: "square",
-                type: "stackedArea",
-                markerSize: 0,
-              color :"rgba(95,53,87,.8)",
-
-                dataPoints: [
-                {x: new Date(2013, 02 , 17) , y: 3.3  },
-                {x: new Date(2013, 02 , 18) , y: 1.6  },
-                {x: new Date(2013, 02 , 19) , y: 2.1  },
-                {x: new Date(2013, 02 , 20) , y: 1.6  },
-                {x: new Date(2013, 02 , 21) , y: 1.4  },
-                {x: new Date(2013, 02 , 22) , y: 1.7  },
-                {x: new Date(2013, 02 , 23) , y: 4.6  }
-
-                ]
-            },
+								{  
+								name: "very distracting",
+								showInLegend: true,
+								legendMarkerType: "square",
+								type: "stackedArea",
+								color :"rgba(211,19,14,.8)",
+								markerSize: 0,
+								dataPoints: [
+									{x: new Date(2013, 02 , 17) , y: 2.4  },
+									{x: new Date(2013, 02 , 18) , y: .6  },
+									{x: new Date(2013, 02 , 19) , y: .8  },
+									{x: new Date(2013, 02 , 20) , y: 1.6  },
+									{x: new Date(2013, 02 , 21) , y: 1.4  },
+									{x: new Date(2013, 02 , 22) , y: 1.4  },
+									{x: new Date(2013, 02 , 23) , y: 2.6  }
+								]
+								},
+								{        
+								name: "distracting",
+								showInLegend: true,
+								legendMarkerType: "square",
+								type: "stackedArea",
+								markerSize: 0,
+								color :"rgba(95,53,87,.8)",
+								dataPoints: [
+									{x: new Date(2013, 02 , 17) , y: 3.3  },
+									{x: new Date(2013, 02 , 18) , y: 1.6  },
+									{x: new Date(2013, 02 , 19) , y: 2.1  },
+									{x: new Date(2013, 02 , 20) , y: 1.6  },
+									{x: new Date(2013, 02 , 21) , y: 1.4  },
+									{x: new Date(2013, 02 , 22) , y: 1.7  },
+									{x: new Date(2013, 02 , 23) , y: 4.6  }
+								]
+								},
             
-            {  
-                name: "productive",
-                showInLegend: true,
-                legendMarkerType: "square",
-                type: "stackedArea",
-                markerSize: 0,
-              color: "rgba(60,84,151,.9)",
-
-                dataPoints: [
-                {x: new Date(2013, 02 , 17) , y: 2.4  },
-                {x: new Date(2013, 02 , 18) , y:  2 },
-                {x: new Date(2013, 02 , 19) , y: 2.8  },
-                {x: new Date(2013, 02 , 20) , y: 1.6  },
-                {x: new Date(2013, 02 , 21) , y: 1.4  },
-                {x: new Date(2013, 02 , 22) , y: 1.4  },
-                {x: new Date(2013, 02 , 23) , y: 1.6  }
-
-                ]
-            },
-            {  
-                name: "very productive",
-                showInLegend: true,
-                legendMarkerType: "square",
-                type: "stackedArea",
-                markerSize: 0,
-              color: "rgba(22,115,211,.9)",
-
-                dataPoints: [
-                {x: new Date(2013, 02 , 17) , y: .4  },
-                {x: new Date(2013, 02 , 18) , y: 7 },
-                {x: new Date(2013, 02 , 19) , y: 6.8  },
-                {x: new Date(2013, 02 , 20) , y: 4.6  },
-                {x: new Date(2013, 02 , 21) , y: 6.4  },
-                {x: new Date(2013, 02 , 22) , y: 7.4  },
-                {x: new Date(2013, 02 , 23) , y: 1.6  }
-
-                ]
-            }
-
-
-
-
-            ]
-        });
-
-chart.render();
-}
-</script>
+								{  
+								name: "productive",
+								showInLegend: true,
+								legendMarkerType: "square",
+								type: "stackedArea",
+								markerSize: 0,
+								color: "rgba(60,84,151,.9)",
+								dataPoints: [
+									{x: new Date(2013, 02 , 17) , y: 2.4  },
+									{x: new Date(2013, 02 , 18) , y:  2 },
+									{x: new Date(2013, 02 , 19) , y: 2.8  },
+									{x: new Date(2013, 02 , 20) , y: 1.6  },
+									{x: new Date(2013, 02 , 21) , y: 1.4  },
+									{x: new Date(2013, 02 , 22) , y: 1.4  },
+									{x: new Date(2013, 02 , 23) , y: 1.6  }
+								]
+								},
+								{  
+								name: "very productive",
+								showInLegend: true,
+								legendMarkerType: "square",
+								type: "stackedArea",
+								markerSize: 0,
+								color: "rgba(22,115,211,.9)",
+								dataPoints: [
+									{x: new Date(2013, 02 , 17) , y: .4  },
+									{x: new Date(2013, 02 , 18) , y: 7 },
+									{x: new Date(2013, 02 , 19) , y: 6.8  },
+									{x: new Date(2013, 02 , 20) , y: 4.6  },
+									{x: new Date(2013, 02 , 21) , y: 6.4  },
+									{x: new Date(2013, 02 , 22) , y: 7.4  },
+									{x: new Date(2013, 02 , 23) , y: 1.6  }
+								]
+								}
+								]
+							});
+							chart.render();
+						}
+					</script>
 					<div id="chart2Container" style="height: 300px; width: 100%;">
+					</div>
 				</div>
 			</div>
 		</div>
