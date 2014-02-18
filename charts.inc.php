8cddca19aed2cf3d646b539ab3961b215f4ae0ba
RevREB <richardebaker@gmail.com>
2014-02-17 19:34:24 -0800

    lets try this

--- charts.inc.php
+++ charts.inc.php
@@ -1,88 +1,61 @@
-<div class="row">
-	<div class="small-12 columns">
-		<div class="row">
-			<div class="small-8 columns">
-				<div class="panel">
-					<p>5s circle</p>
-					<script type="text/javascript">
-  window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer",
-    {
-      title:{
-        text: "Top U.S Smartphone Operating Systems By Market Share, Q3 2012"
-      },
-      data: [
-      {
-       type: "doughnut",
-       dataPoints: [
-       {  y: 53.37, indexLabel: "Android" },
-       {  y: 35.0, indexLabel: "Apple iOS" },
-       {  y: 7, indexLabel: "Blackberry" },
-       {  y: 2, indexLabel: "Windows Phone" },
-       {  y: 5, indexLabel: "Others" },
-       ]
-     }
-     ]
-   });
-
-    chart.render();
-  }
-  </script>
-					<div id="chartContainer" style="height: 300px; width: 100%;">
-					</div>
-					<a href="http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/pie/search_engine_share/">Chart Source</a>
-				</div>
-			</div>
-			<div class="small-4 columns">
-				<div class="panel">
-					<p>Charts will go here</p>
-					<p>Charts will go here</p>
-					<p>Charts will go here</p>
-					<p>Charts will go here</p>
-					<p>Charts will go here</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="small-12 columns">
-				<div class="panel">
-					<p>24h stack chart</p>
-					<script type="text/javascript">
-						window.onload = function () {
-							var chart2 = new CanvasJS.Chart("chart2Container",
-							{
-							title: {
-								text: "24hr Global Mining"             
-							},
-							axisX:{      
-								valueFormatString: "DDD",
-								labelAngle: -40
-							},
-							legend: {
-								verticalAlign: "bottom",
-								horizontalAlign: "center"
-							},
-							toolTip: {
-								content: function(e){
-									var weekday =["-6","-5", "-4", "-3", "-2","-1","Today"];
-									var  str1 = weekday[e.entries[0].dataPoint.x.getDay()] + "<br/>  <span style =' color:" + e.entries[0].dataSeries.color + "';>" +  e.entries[0].dataSeries.name + "</span>: <strong>"+ e.entries[0].dataPoint.y + " hrs</strong> <br/>" ; 
-									return str1
-								}
-							},
-							data: [
-								{  
-								name: "very distracting",
-								showInLegend: true,
-								legendMarkerType: "square",
-								type: "stackedArea",
-								color :"rgba(211,19,14,.8)",
-								markerSize: 0,
-								dataPoints: [
-									{x: new Date(2013, 02 , 17) , y: 2.4  },
-									{x: new Date(2013, 02 , 18) , y: .6  },
-									{x: new Date(2013, 02 , 19) , y: .8  },
-									{x: new Date(2013, 02 , 20) , y: 1.6  },
-									{x: new Date(2013, 02 , 21) , y: 1.4  },
+<script type="text/javascript">
+window.onload = function () {
+	var chart1 = new CanvasJS.Chart("chart1Container",{
+		title:{
+			text: "Top U.S Smartphone Operating Systems By Market Share, Q3 2012"
+		},
+		data: [{
+			type: "doughnut",
+			dataPoints: [{
+				y: 53.37, indexLabel: "Android"
+			},{
+				y: 35.0, indexLabel: "Apple iOS"
+			},{
+				y: 7, indexLabel: "Blackberry"
+			},{
+				y: 2, indexLabel: "Windows Phone"
+			},{
+				y: 5, indexLabel: "Others"
+			}, ]
+		}]
+	});
+    chart1.render();
+	chart1 ={};
+	
+	var chart2 = new CanvasJS.Chart("chart2Container",{
+		title: {
+			text: "24hr Global Mining"             
+		},
+		axisX:{      
+			valueFormatString: "DDD",
+			labelAngle: -40
+		},
+		legend: {
+			verticalAlign: "bottom",
+			horizontalAlign: "center"
+		},
+		toolTip: {
+			content: function(e){
+				var weekday =["-6","-5", "-4", "-3", "-2","-1","Today"];
+				var  str1 = weekday[e.entries[0].dataPoint.x.getDay()] + "<br/>  <span style =' color:" + e.entries[0].dataSeries.color + "';>" +  e.entries[0].dataSeries.name + "</span>: <strong>"+ e.entries[0].dataPoint.y + " hrs</strong> <br/>" ; 
+				return str1}
+		},
+		data: [{  
+			name: "very distracting",
+			showInLegend: true,
+			legendMarkerType: "square",
+			type: "stackedArea",
+			color :"rgba(211,19,14,.8)",
+			markerSize: 0,
+			dataPoints: [{
+				x: new Date(2013, 02 , 17) , y: 2.4
+			},{
+				x: new Date(2013, 02 , 18) , y: .6
+			},{
+				x: new Date(2013, 02 , 19) , y: .8
+			},{
+				x: new Date(2013, 02 , 20) , y: 1.6
+			},{x: new Date(2013, 02 , 21) , y: 1.4  },
 									{x: new Date(2013, 02 , 22) , y: 1.4  },
 									{x: new Date(2013, 02 , 23) , y: 2.6  }
 								]
@@ -141,9 +114,37 @@
 								}
 								]
 							});
-							chart2.render();
-						}
-					</script>
+	chart2.render();
+	chart2 = {};
+  }
+  </script>
+  
+<div class="row">
+	<div class="small-12 columns">
+		<div class="row">
+			<div class="small-8 columns">
+				<div class="panel">
+					<p>5s circle</p>
+					
+					<div id="chartContainer" style="height: 300px; width: 100%;">
+					</div>
+					<a href="http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/pie/search_engine_share/">Chart Source</a>
+				</div>
+			</div>
+			<div class="small-4 columns">
+				<div class="panel">
+					<p>Charts will go here</p>
+					<p>Charts will go here</p>
+					<p>Charts will go here</p>
+					<p>Charts will go here</p>
+					<p>Charts will go here</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-12 columns">
+				<div class="panel">
+					<p>24h stack chart</p>
 					<div id="chart2Container" style="height: 300px; width: 100%;">
 					</div>
 					<a href="http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/area/productivity_by_day/">Chart Source</a>
