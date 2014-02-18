e32553cd3c962a9e4c3620fd767b7ee431e4f383
RevREB <richardebaker@gmail.com>
2014-02-17 17:09:42 -0800

    Charts formating

--- charts.inc.php
+++ charts.inc.php
@@ -1,5 +1,21 @@
 <div class="row">
 	<div class="small-12 columns" role="content">
-		<p>Charts will go here</p>
+		<div class="row">
+			<div class="small-6 columns">
+				<div class="panel">
+					<p>5s circle</p>
+				</div>
+			</div>
+			<div class="small-6 columns">
+				<div class="panel">
+					<p>Charts will go here</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="panel">
+				<p>24h stack chart</p>
+			</div>
+		</div>
     </div>
 </div>
