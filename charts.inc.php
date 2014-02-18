9eea5dd9f976af64f5b01c601dbe544a6279ecd4
RevREB <richardebaker@gmail.com>
2014-02-17 17:11:46 -0800

    chart equalizer

--- charts.inc.php
+++ charts.inc.php
@@ -1,18 +1,18 @@
 <div class="row">
 	<div class="small-12 columns" role="content">
 		<div class="row">
-			<div class="small-6 columns">
+			<div class="small-6 columns" data-equalizer-watch>
 				<div class="panel">
 					<p>5s circle</p>
 				</div>
 			</div>
-			<div class="small-6 columns">
+			<div class="small-6 columns"data-equalizer-watch>
 				<div class="panel">
 					<p>Charts will go here</p>
 				</div>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" data-equalizer-watch>
 			<div class="panel">
 				<p>24h stack chart</p>
 			</div>
