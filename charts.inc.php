10d3e1f6de85bcd3f804b389f1d1b26a1a5a2661
RevREB <richardebaker@gmail.com>
2014-02-17 18:54:45 -0800

    timbits

--- charts.inc.php
+++ charts.inc.php
@@ -31,7 +31,7 @@
 						chart.render();
 						}
 					</script>
-					<div id="chartContainer" style="height: 300px; width: 100%;">
+					<div id="chartContainer">
 					</div>
 					<a href="http://canvasjs.com/editor/?id=http://canvasjs.com/example/gallery/pie/search_engine_share/">Chart Source</a>
 				</div>
